87e14cfcf6c43d38ac1ba7832be14930fba07bb5
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-07-09 16:31:40 -0400

    feat: convar

--- client/core/interfaces/interfaces.cpp
+++ client/core/interfaces/interfaces.cpp
@@ -6,25 +6,38 @@
 
 bool interfaces_t::collect_interfaces() {
 
-  client::g_console.printf("\tmodules:", console_color_beige);
+  client::g_console.printf("\tmodules:", console_color_light_yellow);
   pe::module_t shaderapidx9_dll;
   if (!pe::get_module("shaderapidx9.dll", shaderapidx9_dll)) {
-    client::g_console.print("\t\tfailed to find shader api",
-                            console_colors_e::console_color_red);
+    client::g_console.print("\t\tfailed to find shader api", console_color_red);
     return false;
   }
-  client::g_console.print("\t\tfound shader api", console_color_cyan);
+  client::g_console.print("\t\tfound shader api", console_color_light_aqua);
 
-  client::g_console.printf("\taddresses:", console_color_beige);
+  pe::module_t vstdlib_dll;
+  if (!pe::get_module("vstdlib.dll", vstdlib_dll)) {
+    client::g_console.print("Failed to find vstdlib", console_color_red);
+    return false;
+  }
+  client::g_console.print("\t\tfound vstdlib", console_color_light_aqua);
+
+  client::g_console.printf("\taddresses:", console_color_light_yellow);
   this->d3d9_device = *(shaderapidx9_dll.find_pattern_in_memory("48 89 1D ?? ?? ?? ?? 48 8B CF")
                             .rel32<IDirect3DDevice9**>(0x3));
   if (!this->d3d9_device) {
     client::g_console.print("\t\tfailed to find d3d9 device", console_color_red);
     return false;
   }
-  client::g_console.print("\t\tfound d3d9 device", console_color_cyan);
+  client::g_console.print("\t\tfound d3d9 device", console_color_light_aqua);
 
-  client::g_console.printf("\tinterfaces:", console_color_beige);
+  client::g_console.printf("\tinterfaces:", console_color_light_yellow);
+
+  this->cvar = vstdlib_dll.get_interface<cvar_t*>(HASH("VEngineCvar004"));
+  if (!this->cvar) {
+    client::g_console.print("Failed to find cvar", console_color_red);
+    return false;
+  }
+  client::g_console.print("\t\tfound cvar", console_color_light_aqua);
 
   return true;
 }
--- client/core/interfaces/interfaces.h
+++ client/core/interfaces/interfaces.h
@@ -2,9 +2,12 @@
 
 class IDirect3DDevice9;
 
+class cvar_t;
+
 class interfaces_t {
 public:
   IDirect3DDevice9* d3d9_device{nullptr};
+  cvar_t*           cvar{nullptr};
 
   bool collect_interfaces();
 };
