e93ba6371c78907a89c7a7cac7e6a14d53668314
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-07-19 13:39:04 -0400

    crash fixed, offsets now initialized before being used

--- client/client.cpp
+++ client/client.cpp
@@ -5,16 +5,20 @@
 #include <Windows.h>
 #include <d3d9.h>
 #include "valve/cusercmd.h"
-#include "valve/client_local_player.h"
-#include "valve/model_info.h"
-#include "valve/studio_header.h"
-#include "valve/global_vars_base.h"
 
 bool client::initialize() {
   g_console.open_console();
 
   client::g_console.print("initializing interfaces", console_color_white);
   if (!g_interfaces.collect_interfaces())
+    return false;
+
+  client::g_console.print("initializing global addresses", console_color_white);
+  if (!g_addresses.collect_addresses())
+    return false;
+
+  client::g_console.print("initializing offsets", console_color_white);
+  if (!g_offsets.get_offsets())
     return false;
 
   get_window_handle();
@@ -28,14 +32,6 @@ bool client::initialize() {
 
   client::g_console.print("initializing hooks", console_color_white);
   if (!g_hooks.initialize())
-    return false;
-
-  client::g_console.print("initializing global addresses", console_color_white);
-  if (!g_addresses.collect_addresses())
-    return false;
-
-  client::g_console.print("initializing offsets", console_color_white);
-  if (!g_offsets.get_offsets())
     return false;
 
   attached = true;
@@ -88,40 +84,9 @@ void client::on_present() {
   g_render.finish();
 }
 
-void test() {
-  client_local_player_t* local_player = client_base_entity_t::get_local_player();
-  if (!local_player || !local_player->is_alive())
-    return;
-
-  const model_t* model = local_player->get_model();
-  if (!model)
-    return;
-  client::g_console.print("got model!");
-
-  studio_header_t* header = client::g_interfaces.model_info->get_studio_model(model);
-  if (!header)
-    return;
-  client::g_console.printf("header pointer: %p", header);
-
-  int hitbox_set_index = local_player->hitbox_set();
-  client::g_console.printf("hitbox set index: %d", hitbox_set_index);
-
-  studio_hitbox_set_t* hitbox_set_ptr = header->get_hitbox_set(hitbox_set_index);
-  if (!hitbox_set_ptr)
-    return;
-  client::g_console.printf("hitbox set ptr: %p", hitbox_set_ptr);
-
-  studio_bbox_t* hitbox = header->get_hitbox(12, hitbox_set_index);
-  if (!hitbox)
-    return;
-  client::g_console.printf("hitbox pointer: %p", hitbox);
-}
-
 bool client::on_create_move(usercmd_t* cmd) {
   if (!cmd || !cmd->command_number)
     return false;
-
-  test();
 
   g_prediction.start_prediction(cmd);
 
