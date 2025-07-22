ecefa7977479e635d18a372d9fb92a77e14f2727
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-07-22 04:29:10 -0400

    update model_render class

--- valve/model_render.h
+++ valve/model_render.h
@@ -1,5 +1,16 @@
 #pragma once
 
+enum override_type_e {
+  OVERRIDE_NORMAL = 0,
+  OVERRIDE_BUILD_SHADOWS,
+  OVERRIDE_DEPTH_WRITE,
+  OVERRIDE_SSAO_DEPTH_WRITE,
+};
+
+class material_t;
 class model_render_t {
 public:
+  virtual int  vfunc00()                                                                 = 0;
+  virtual void forced_material_override(material_t*     new_material,
+                                        override_type_e override_type = OVERRIDE_NORMAL) = 0;
 };
