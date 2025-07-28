7999fda421b31b6c6e5a7a3fe998297b8e310da6
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-07-28 00:33:46 -0400

    add qangle_t

--- library/math.cpp
+++ library/math.cpp
@@ -5,6 +5,12 @@
 #include "valve/vector2.h"
 #include "client/client.h"
 #include "valve/matrix3x4.h"
+#include "valve/qangle.h"
+
+void sin_cos(float radians, float* sine, float* cosine) {
+  *sine   = sinf(radians);
+  *cosine = cosf(radians);
+}
 
 bool math::world_to_screen(const vector3_t& origin, vector2_t& screen_position,
                            view_matrix_t& matrix) {
@@ -34,4 +40,14 @@ vector3_t math::vector_transform(matrix_3x4_t& matrix, vector3_t offset) {
   return vector3_t{offset.dot(vector3_t(matrix[0])) + matrix[0][3],
                    offset.dot(vector3_t(matrix[1])) + matrix[1][3],
                    offset.dot(vector3_t(matrix[2])) + matrix[2][3]};
+}
+
+void math::angle_vectors(const qangle_t& angles, vector3_t* forward) {
+  float sp, sy, cp, cy;
+  sin_cos(degrees_to_radian(angles[1]), &sy, &cy);
+  sin_cos(degrees_to_radian(angles[0]), &sp, &cp);
+
+  forward->x = cp * cy;
+  forward->y = cp * sy;
+  forward->z = -sp;
 }
