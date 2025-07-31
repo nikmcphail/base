6db733d85c10c950989e99462de7137f3d112396
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-07-30 20:36:06 -0400

    allow different bodies for atlas materials

--- client/core/atlas/atlas.cpp
+++ client/core/atlas/atlas.cpp
@@ -4,10 +4,10 @@
 #include "valve/material_system.h"
 #include "client/client.h"
 
-material_t* create_material(const std::string& name, const std::string& shader) {
-  key_values_t* kv = new key_values_t(name.c_str());
-
-  std::string buffer = "\"" + shader + "\"\n{\n}\n";
+material_t* create_material(const std::string& name, const std::string& shader,
+                            std::string body = "") {
+  key_values_t* kv     = new key_values_t(name.c_str());
+  std::string   buffer = "\"" + shader + "\"\n{\n" + body + "\n}";
   kv->load_from_buffer(name.c_str(), buffer.c_str());
 
   material_t* mat = client::g_interfaces.material_system->create_material(name.c_str(), kv);
@@ -24,7 +24,8 @@ material_t* create_material(const std::string& name, const std::string& shader) 
 }
 
 bool atlas_t::initialize() {
-  test_material = create_material("test_material", "VertexLitGeneric");
+  test_material =
+      create_material("test_material", "VertexLitGeneric", "\t\"$basetexture\" \"vgui/white\"");
   if (!test_material)
     return false;
 
