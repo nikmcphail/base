0e669443610e562880d99b9592431335370054da
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-08-02 02:42:44 -0400

    start work on lag compensation

---
+++ valve/lagcompensation/lag_record.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "valve/vector3.h"
+#include "valve/qangle.h"
+
+#define MAX_LAYER_RECORDS  15
+#define MAXSTUDIOPOSEPARAM 24
+
+struct layer_record_t {
+  int   sequence;
+  float cycle;
+  float weight;
+  int   order;
+
+  layer_record_t() {
+    sequence = 0;
+    cycle    = 0;
+    weight   = 0;
+    order    = 0;
+  }
+
+  layer_record_t(const layer_record_t& src) {
+    sequence = src.sequence;
+    cycle    = src.cycle;
+    weight   = src.weight;
+    order    = src.order;
+  }
+};
+
+struct lag_record_t {
+public:
+  lag_record_t() {
+    flags = 0;
+    origin.initialize();
+    angles.initialize();
+    mins_pre_scaled.initialize();
+    maxs_pre_scaled.initialize();
+    simulation_time = -1;
+    master_sequence = 0;
+    master_cycle    = 0;
+
+    for (int i = 0; i < MAXSTUDIOPOSEPARAM; i++) {
+      pose_parameters[i] = 0;
+    }
+  }
+
+  lag_record_t(const lag_record_t& src) {
+    flags           = src.flags;
+    origin          = src.origin;
+    angles          = src.angles;
+    mins_pre_scaled = src.mins_pre_scaled;
+    maxs_pre_scaled = src.maxs_pre_scaled;
+    simulation_time = src.simulation_time;
+
+    for (int layer_index = 0; layer_index < MAX_LAYER_RECORDS; ++layer_index) {
+      layer_records[layer_index] = src.layer_records[layer_index];
+    }
+
+    master_sequence = src.master_sequence;
+    master_cycle    = src.master_cycle;
+
+    for (int i = 0; i < MAXSTUDIOPOSEPARAM; i++) {
+      pose_parameters[i] = src.pose_parameters[i];
+    }
+  }
+
+  int            flags;
+  vector3_t      origin;
+  qangle_t       angles;
+  vector3_t      mins_pre_scaled;
+  vector3_t      maxs_pre_scaled;
+  float          simulation_time;
+  layer_record_t layer_records[MAX_LAYER_RECORDS];
+  int            master_sequence;
+  float          master_cycle;
+  float          pose_parameters[MAXSTUDIOPOSEPARAM];
+};
